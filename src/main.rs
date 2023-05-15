7f8a08550f6b879999883e398c28a1e6961c85bd
Adrian Reghitto <94520482+adre5577@users.noreply.github.com>
2023-05-15 14:36:38 -0600

    Implemented program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std;
+use std::fs;
+use std::env;
+use std::path::Path;
+use blake3;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    if args.len() < 3 {
+        println!("Usage: b3rn [number of bytes] [files]");
+    }
+
+    args.next();
+
+    let bytes = args.next().unwrap().parse::<usize>().expect("A number must be provided.");
+    for fnstring in args {
+        let filename = fnstring.as_str();
+        let filedata = match fs::read(filename) {
+            Ok(data) => data,
+            Err(err) => {
+                println!("Error reading {filename}: {err}");
+                continue;
+            }
+        };
+
+        let mut hasher = blake3::Hasher::new();
+        hasher.update(&filedata);
+        let mut output = hasher.finalize_xof();
+
+        let mut bName = vec![0; bytes];
+        output.fill(&mut bName);
+
+        let mut new_name = String::new();
+        for byte in bName {
+            new_name += &format!("{:x}", byte);
+        }
+    
+        if let Some(extension) = Path::new(filename).extension() {
+            new_name += ".";
+            new_name += extension.to_str().expect("Non-unicode filename.");
+        }
+
+        match fs::rename(filename, new_name) {
+            Ok(_) => {},
+            Err(err) => println!("Error renaming {filename}: {err}")
+        };
+    }
 }
